5e60e7c615de078c23d5bc44cf35b32ab81829bf
Rhoda Pickens <rhoda@Mac.lan>
2026-06-18 17:51:34 -0500

    Initial commit

---
+++ backend/src/test/java/com/mealplanner/meal_planner_backend/MealPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealplanner.meal_planner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
